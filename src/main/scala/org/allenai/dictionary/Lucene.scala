e40d333fca753aaf6af820319a2eedd7f0d3fa09
Anthony Fader <afader@gmail.com>
2014-12-16 08:26:29 -0800

    finished lucene hackign

--- src/main/scala/org/allenai/dictionary/Lucene.scala
+++ src/main/scala/org/allenai/dictionary/Lucene.scala
@@ -1,7 +1,112 @@
 package org.allenai.dictionary
 
-object LuceneIndex {
-  
-  def add(document: Seq[WordWithCluster]): Unit = ???
+import scala.collection.JavaConverters._
+import org.apache.lucene.util.Version
+import org.apache.lucene.document.FieldType
+import java.io.File
+import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper
+import org.apache.lucene.index.IndexWriterConfig
+import org.apache.lucene.index.IndexWriter
+import org.apache.lucene.store.NIOFSDirectory
+import org.apache.lucene.document.Document
+import org.apache.lucene.document.Field
+import org.apache.lucene.index.DirectoryReader
+import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.util.Bits
+import org.apache.lucene.search.spans.SpanQuery
+import scala.collection.mutable.ListBuffer
+import java.util.HashMap
+import org.apache.lucene.index.Term
+import org.apache.lucene.index.TermContext
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute
+import org.apache.lucene.search.highlight.TokenSources
+import org.apache.lucene.sandbox.queries.regex.RegexQuery
+import org.apache.lucene.search.spans.SpanMultiTermQueryWrapper
+import org.apache.lucene.index.FieldInfo
+import org.apache.lucene.search.spans.SpanTermQuery
+import org.apache.lucene.search.spans.SpanNearQuery
 
+case class LuceneReader(path: File) {
+  import Lucene._
+  val directory = new NIOFSDirectory(path)
+  val reader = DirectoryReader.open(directory)
+  val searcher = new IndexSearcher(reader)
+  val arc = reader.getContext.leaves().get(0)
+  val bits = new Bits.MatchAllBits(reader.numDocs)
+  
+  def clusterPrefixQuery(prefix: String): SpanQuery = {
+    val queryString = s"${clusterTokenPrefix}${prefix}.*"
+    val query = new RegexQuery(new Term(clusterFieldName, queryString))
+    val rewritten = new SpanMultiTermQueryWrapper(query).rewrite(reader)
+    rewritten.asInstanceOf[SpanQuery]
+  }
+  
+  def wordQuery(word: String): SpanQuery = new SpanTermQuery(new Term(clusterFieldName, word))
+  
+  def sequenceQuery(qs: Seq[SpanQuery]): SpanQuery = new SpanNearQuery(qs.toArray, 0, true)
+  
+  def matches(spanQuery: SpanQuery): Iterable[String] = {
+    val results = new ListBuffer[String]
+    val termContexts = new HashMap[Term, TermContext]
+    val spans = spanQuery.getSpans(arc, bits, termContexts)
+    while (spans.next) {
+      val id = spans.doc
+      val start = spans.start()
+      val end = spans.end()
+      val doc = searcher.doc(id)
+      val tokenStream = TokenSources.getAnyTokenStream(reader, id, fieldName, plainAnalyzer)
+      val charTermAttribute = tokenStream.addAttribute(charTermAttrClass)
+      var terms = new ListBuffer[String]
+      var pos = 0
+      while (tokenStream.incrementToken) {
+        if (start <= pos && pos < end) {
+          val term = charTermAttribute.toString
+          terms += term
+        }
+        pos += 1
+      }
+      val matched = terms.mkString(" ")
+      results += matched 
+    }
+    results.toSeq
+  }
+}
+
+case class LuceneWriter(path: File, wordClusters: Map[String, String]) {
+  import Lucene._
+  val clusterAnalyzer = new ClusterAnalyzer(wordClusters)
+  val analyzerMap = Map(fieldName -> plainAnalyzer, clusterFieldName -> clusterAnalyzer)
+  val analyzer = new PerFieldAnalyzerWrapper(plainAnalyzer, analyzerMap.asJava)
+  val directory = new NIOFSDirectory(path)
+  val config = new IndexWriterConfig(version, analyzer)
+  def writer = new IndexWriter(directory, config)
+  def makeDoc(text: String): Document = {
+    val doc = new Document
+    doc.add(new Field(fieldName, text, fieldType))
+    doc.add(new Field(clusterFieldName, text, fieldType))
+    doc
+  }
+  def write(texts: Iterator[String]): Unit = {
+    val w = writer
+    val docs = texts map makeDoc foreach w.addDocument 
+    w.commit
+  }
+}
+
+object Lucene {
+  val version = Version.LUCENE_48
+  val fieldName = "text"
+  val clusterFieldName = s"${fieldName}Clusters"
+  val fieldType = {
+    val f = new FieldType
+    f.setIndexed(true)
+    f.setStored(true)
+    f.setStoreTermVectors(true)
+    f.setStoreTermVectorOffsets(true)
+    f.freeze
+    f
+  }
+  val clusterTokenPrefix = "CLUSTER"
+  val charTermAttrClass = classOf[CharTermAttribute]
+  val plainAnalyzer = new PlainAnalyzer
 }
--- src/test/scala/org/allenai/dictionary/TestLucene.scala
+++ src/test/scala/org/allenai/dictionary/TestLucene.scala
@@ -57,276 +57,31 @@ import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper
 import org.apache.lucene.search.spans.SpanMultiTermQueryWrapper
 import org.apache.lucene.sandbox.queries.regex.RegexQuery
 
-class ClusterTokenFilter(stream: TokenStream, wordClusters: Map[String, String]) extends TokenFilter(stream) {
-  
-  val charTermAttr = addAttribute(classOf[CharTermAttribute])
-  val posIncrAttr = addAttribute(classOf[PositionIncrementAttribute]);
-  var buffer: Option[String] = None
-  
-  override def incrementToken: Boolean = {
-    if (buffer.isDefined) {
-      posIncrAttr.setPositionIncrement(0)
-      charTermAttr.setEmpty.append(s"CLUSTER=${buffer.get}")
-      buffer = None
-      return true
-    }
-    if (!input.incrementToken) {
-      return false
-    }
-    buffer = wordClusters.get(charTermAttr.toString)
-    return true
-  }
-  
-  override def reset {
-    super.reset
-    buffer = None
-  }
-  
-}
+
 
 class TestLucene extends FlatSpec {
+      
+  val docs = "This is some super text that is super slick." :: "This is super salty!" :: Nil
+  val wordClusters = Map("super" -> "00", "slick" -> "01", "salty" -> "001", "text" -> "1")
+    
+  val tempDir = Files.createTempDirectory("lucene").toFile
+  val writer = LuceneWriter(tempDir, wordClusters)
+  writer.write(docs.iterator)
+  val reader = LuceneReader(tempDir)
   
-  "Lucene" should "do its thang" in {
-    
-	val tempDir = Files.createTempDirectory("lucene").toFile
-	val version = Version.LUCENE_48
-    val directory = new NIOFSDirectory(tempDir)
-	val wordClusters = Map("super" -> "00", "slick" -> "01", "this" -> "1", "is" -> "10")
-	val fieldName = "fieldname"
-	val clusterFieldName = s"${fieldName}Clusters"
-	
-    val clusterAnalyzer = new Analyzer {
-	  def createComponents(fieldName: String, reader: Reader): TokenStreamComponents = {
-	    val source = new StandardTokenizer(version, reader)
-	    val lowercase = new LowerCaseFilter(version, source)
-	    val filter = new ClusterTokenFilter(lowercase, wordClusters)
-	    new TokenStreamComponents(source, filter)
-	  }
-	}
-	
-	val plainAnalyzer = new Analyzer {
-	  def createComponents(fieldName: String, reader: Reader): TokenStreamComponents = {
-	    val source = new StandardTokenizer(version, reader)
-	    val lowercase = new LowerCaseFilter(version, source)
-	    new TokenStreamComponents(source, lowercase)
-	  }
-	}
-	
-	val analyzerMap = Map(fieldName -> plainAnalyzer, clusterFieldName -> clusterAnalyzer)
-	val analyzer = new PerFieldAnalyzerWrapper(plainAnalyzer, analyzerMap.asJava)
-	
-	val config = new IndexWriterConfig(version, analyzer)
-	val iwriter = new IndexWriter(directory, config)
-	
-	val doc = new Document
-	val text = "This is some super text that is super slick."
-	
-	val fieldType = new FieldType
-	fieldType.setIndexed(true)
-	fieldType.setStored(true)
-	fieldType.setStoreTermVectors(true)
-    fieldType.setStoreTermVectorOffsets(true)
-	
-	doc.add(new Field(clusterFieldName, text, fieldType))
-	doc.add(new Field(fieldName, text, fieldType))
-	iwriter.addDocument(doc)
-	iwriter.commit
-	iwriter.close
-	
-	
-	val ireader = DirectoryReader.open(directory)
-	val isearcher = new IndexSearcher(ireader)
-	
-	val x = new RegexQuery(new Term(clusterFieldName, "CLUSTER=00"))
-	val y = (new SpanMultiTermQueryWrapper(x).rewrite(ireader)).asInstanceOf[SpanQuery]
-	
-	
-	val spanTerms: Array[SpanQuery] = Array(y, new SpanTermQuery(new Term(clusterFieldName, "slick")))
-	val spanQuery = new SpanNearQuery(spanTerms, 0, true)
-	
-	val arc = ireader.getContext.leaves().get(0)
-	val termContexts = new HashMap[Term, TermContext]()
-	val bits = new Bits.MatchAllBits(ireader.numDocs())
-	
-	val spans = spanQuery.getSpans(arc, bits, termContexts)
-    while (spans.next) {
-      val id = spans.doc
-      val start = spans.start()
-      val end = spans.end()
-      val doc = isearcher.doc(id)
-      val tokenStream = TokenSources.getAnyTokenStream(ireader, id, fieldName, analyzer)
-      val charTermAttribute = tokenStream.addAttribute(classOf[CharTermAttribute])
-      var terms = new ListBuffer[String]
-      while (tokenStream.incrementToken) {
-        val term = charTermAttribute.toString
-        terms += term
-      }
-      println(terms.slice(start,end) mkString(" "))
-	}
-	
-	
-	/*val parser = new QueryParser(version, fieldName, analyzer)
-    val query = parser.parse("""super slick""")
-    val ireader = DirectoryReader.open(directory)
-    val isearcher = new IndexSearcher(ireader)
-    val topDocs = isearcher.search(query, null, 1000)
-    
-    for (scoreDoc <- topDocs.scoreDocs) {
-	  val id = scoreDoc.doc
-	  val hitDoc = isearcher.doc(id)
-	  val tokenStream = TokenSources.getAnyTokenStream(ireader, id, fieldName, analyzer)
-	  val charTermAttribute = tokenStream.addAttribute(classOf[CharTermAttribute])
-	  val offsetAttribute = tokenStream.addAttribute(classOf[OffsetAttribute])
-	  while (tokenStream.incrementToken) {
-	    println(offsetAttribute.startOffset + " " + charTermAttribute.toString)
-	  }
-    }*/
-	
-	
-	
-	
+  "Lucene" should "search over words and clusters" in {
+    val x = reader.wordQuery("super")
+	val y = reader.clusterPrefixQuery("0")
+	val q = reader.sequenceQuery(x :: y :: Nil)
+	val results = reader.matches(q).toSet
+	assert(results == Set("super slick", "super salty"))
   }
   
-  
-  
-  
-  
-  
-  
-  
-  
-      /*val offsetAttribute = tokenStream.addAttribute(classOf[OffsetAttribute])
-      val charTermAttribute = tokenStream.addAttribute(classOf[CharTermAttribute])
-      tokenStream.reset
-      while (tokenStream.incrementToken) {
-        println(offsetAttribute.startOffset + " " + charTermAttribute.toString)
-      }
-	
-	
-/*	val wordClusters = ("super", "00") :: ("slick", "01") :: Nil  
-	val builder = new SynonymMap.Builder(true)
-	for {
-	  (word, cluster) <- wordClusters
-	  wordChar = new CharsRef(word)
-	  clusterChar = new CharsRef(cluster)
-	} builder.add(wordChar, clusterChar, true)
-	val synonymMap = builder.build()
-	
-
-	val analyzer = new Analyzer {
-	  def createComponents(fieldName: String, reader: Reader): TokenStreamComponents = {
-	    val source = new StandardTokenizer(version, reader)
-	    val lowercase = new LowerCaseFilter(version, source)
-	    val filter = new SynonymFilter(lowercase, synonymMap, true)
-	    new TokenStreamComponents(source, filter)
-	  }
-	}
-
-	val stdAnalyzer = new StandardAnalyzer(version)
-	val directory = new NIOFSDirectory(tempDir)
-	val config = new IndexWriterConfig(version, analyzer)
-	val iwriter = new IndexWriter(directory, config)
-
-	val fieldName = "fieldname"
-	val synFieldName = "fieldnameSyn"
-	val doc = new Document
-	val text = "This is some super text that is super slick."
-	  //Field.TermVector.WITH_POSITIONS_OFFSETS
-	
-	
-	
-	
-	val fieldType = new FieldType
-	fieldType.setIndexed(true)
-	fieldType.setIndexOptions(FieldInfo.IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS)
-	fieldType.setStored(true)
-	fieldType.setStoreTermVectors(true)
-    fieldType.setTokenized(true)
-    fieldType.setStoreTermVectorOffsets(true)
-	
-	
-	doc.add(new Field(fieldName, text, fieldType))
-	doc.add(new Field("foo", text, fieldType, stdAnalyzer))
-	//doc.add(new Field(synFieldName, text, fieldType))
-	iwriter.addDocument(doc)
-	iwriter.close
-
-	val ireader = DirectoryReader.open(directory)
-	val isearcher = new IndexSearcher(ireader)
-
-	val parser = new QueryParser(version, fieldName, analyzer)
-	val query = parser.parse(""" super slick """)
-	
-	val spanTerms: Array[SpanQuery] = Array(new SpanTermQuery(new Term(fieldName, "super")), new SpanTermQuery(new Term(fieldName, "slick")))
-	val spanQuery = new SpanNearQuery(spanTerms, 0, true)
-	
-	
-	val topDocs = isearcher.search(spanQuery, null, 1000)
-	
-	val htmlFormatter = new SimpleHTMLFormatter
-	
-	val matches = new ListBuffer[String]
-	val myFormatter = new Formatter {
-	  def highlightTerm(originalText: String, tokenGroup: TokenGroup): String = {
-	    val x = tokenGroup.getNumTokens
-	    if (tokenGroup.getTotalScore > 0) matches += x + " " + originalText
-	    originalText
-	  }
-	}
-	
-	val arc = ireader.getContext.leaves().get(0)
-	
-	//val arc = isearcher.getTopReaderContext.asInstanceOf[AtomicReaderContext]
-	val termContexts = new HashMap[Term, TermContext]()
-	val bits = new Bits.MatchAllBits(ireader.numDocs())
-	
-	val spans = spanQuery.getSpans(arc, bits, termContexts)
-	val window = 0
-	while (spans.next) {
-      val id = spans.doc
-      val start = spans.start()
-      val end = spans.end()
-      val doc = isearcher.doc(id)
-      val tokenStream = TokenSources.getAnyTokenStream(ireader, id, "foo", stdAnalyzer)
-      val charTermAttribute = tokenStream.addAttribute(classOf[CharTermAttribute])
-      var terms = new ListBuffer[String]
-      while (tokenStream.incrementToken) {
-        val term = charTermAttribute.toString
-        terms += term
-      }
-      println(terms mkString(" "))
-	}
-	
-	
-	/*
-	val highlighter = new Highlighter(myFormatter, new QueryScorer(spanQuery))
-	
-	assert(1 == topDocs.scoreDocs.length)
-	for (scoreDoc <- topDocs.scoreDocs) {
-	  val id = scoreDoc.doc
-	  val hitDoc = isearcher.doc(id)
-      assert(text == hitDoc.get(fieldName))
-      
-      
-      
-      val tokenStream = TokenSources.getAnyTokenStream(ireader, id, fieldName, stdAnalyzer)
-      /*val offsetAttribute = tokenStream.addAttribute(classOf[OffsetAttribute])
-      val charTermAttribute = tokenStream.addAttribute(classOf[CharTermAttribute])
-      tokenStream.reset
-      while (tokenStream.incrementToken) {
-        println(offsetAttribute.startOffset + " " + charTermAttribute.toString)
-      }
-	  tokenStream.reset*/
-      
-      
-      //val frag = highlighter.getBestTextFragments(tokenStream, hitDoc.get(fieldName), false, 4)
-      //matches foreach println
-      
-	}*/
-	* 
-	* 
-	*/
-  }*/
-  
-
+  it should "handle regexes" in {
+    val x = reader.wordQuery("super")
+	val y = reader.clusterPrefixQuery("")
+	val q = reader.sequenceQuery(x :: y :: Nil)
+	val results = reader.matches(q).toSet
+	assert(results == Set("super slick", "super salty", "super text"))
+  }
 }
