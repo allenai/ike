cf1ffc4df26e78a65372bbd7e2ecd7bdfd4677cc
Anthony Fader <afader@gmail.com>
2015-01-11 11:46:51 -0800

    fixed regex parser combinators

--- src/main/scala/org/allenai/dictionary/QueryExpr.scala
+++ src/main/scala/org/allenai/dictionary/QueryExpr.scala
@@ -19,6 +19,7 @@ object WildCard extends QueryExpr {
 }
 
 case class QueryCapture(expr: QueryExpr) extends QueryExpr
+case class QueryNonCapture(expr: QueryExpr) extends QueryExpr
 
 case class QuerySeq(exprs: Seq[QueryExpr]) extends QueryExpr
 case object QuerySeq {
@@ -31,31 +32,43 @@ case object QuerySeq {
 case class ContentRef(name: String) extends QueryExpr
 
 case class QueryDisjunction(parts: Seq[QueryExpr]) extends QueryExpr
+case object QueryDisjunction {
+  def fromSeq(seq: Seq[QueryExpr]): QueryExpr = seq match {
+    case expr :: Nil => expr
+    case _ => QueryDisjunction(seq)
+  }
+}
+
+case class QueryStar(expr: QueryExpr) extends QueryExpr
+case class QueryPlus(expr: QueryExpr) extends QueryExpr
 
 object QueryExprParser extends RegexParsers {
-  
   val posTagSet = Seq("PRP$","NNPS","WRB","WP$","WDT","VBZ","VBP","VBN","VBG","VBD","SYM","RBS",
       "RBR","PRP","POS","PDT","NNS","NNP","JJS","JJR","WP","VB","UH","TO","RP","RB","NN","MD","LS",
       "JJ","IN","FW","EX","DT","CD","CC")
   val posTagRegex = posTagSet.map(Pattern.quote).mkString("|").r
-  
   def leftParen = "("
+  def nonCapLeftParen = "(?:"
   def rightParen = ")"
-    
-  def wildcard: Parser[QueryExpr] = "\\.".r ^^ { _ => WildCard }
-  def content: Parser[Content] = """[^\^$()\s]+""".r ^^ Content
-  def contentRef: Parser[ContentRef] = """\$[^$()\s]+""".r ^^ { s => ContentRef(s.tail) }
-  def clusterPrefix: Parser[ClusterPrefix] = """\^[01]+\b""".r ^^ { s => ClusterPrefix(s.tail) }
-  def posTag: Parser[PosTag] = posTagRegex ^^ PosTag
-  
-  def atom: Parser[QueryExpr] = wildcard | posTag | contentRef | clusterPrefix | content
-  def atoms: Parser[QueryExpr] = rep1(atom) ^^ QuerySeq.fromSeq
-  
-  def capture: Parser[QueryCapture] = leftParen ~> queryExpr <~ rightParen ^^ QueryCapture
-  
-  def queryExpr: Parser[QueryExpr] = rep1(atoms | capture) ^^ QuerySeq.fromSeq
-  
-  def parse(s: String): ParseResult[QueryExpr] = parseAll(queryExpr, s)
+  def pipe = "|"
+  def star = "*"
+  def plus = "+"
+  def wildcard = "\\.".r ^^^ WildCard
+  def content = """[^|\^$()\s*+]+""".r ^^ Content
+  def contentRef = """\$[^$()\s*+|]+""".r ^^ { s => ContentRef(s.tail) }
+  def clusterPrefix = """\^[01]+""".r ^^ { s => ClusterPrefix(s.tail) }
+  def posTag = posTagRegex ^^ PosTag
+  def atom = wildcard | posTag | contentRef | clusterPrefix | content
+  def capture = leftParen ~> expr <~ rightParen ^^ QueryCapture
+  def nonCapture = nonCapLeftParen ~> expr <~ rightParen ^^ QueryNonCapture
+  def operand = nonCapture | capture | atom
+  def starred = operand <~ star ^^ QueryStar
+  def plussed = operand <~ plus ^^ QueryPlus
+  def modified = starred | plussed
+  def piece: Parser[QueryExpr] = (modified | operand)
+  def branch = rep1(piece) ^^ QuerySeq.fromSeq
+  def expr = repsep(branch, pipe) ^^ QueryDisjunction.fromSeq
+  def parse(s: String) = parseAll(expr, s)
 }
 
 object Foo extends App {
