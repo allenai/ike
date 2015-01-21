2379a35697cc55d8763e12d1049aff68ae674b9c
Tony Fader <afader@gmail.com>
2015-01-20 17:57:37 -0800

    added query parser

---
+++ src/main/scala/org/allenai/dictionary/QueryLanguage.scala
@@ -0,0 +1,56 @@
+package org.allenai.dictionary
+
+import scala.util.parsing.input.Positional
+import scala.util.parsing.combinator.RegexParsers
+import java.util.regex.Pattern
+
+sealed trait QExpr extends Positional
+case class QWord(value: String) extends QExpr
+case class QCluster(value: String) extends QExpr
+case class QPos(value: String) extends QExpr
+case class QDict(value: String) extends QExpr
+object QWildcard extends QExpr
+case class QNamed(qexpr: QExpr, name: String) extends QExpr
+case class QUnnamed(qexpr: QExpr) extends QExpr
+case class QNonCap(qexpr: QExpr) extends QExpr
+case class QStar(qexpr: QExpr) extends QExpr
+case class QPlus(qexpr: QExpr) extends QExpr
+case class QSeq(qexprs: Seq[QExpr]) extends QExpr
+case object QSeq {
+  def fromSeq(seq: Seq[QExpr]): QExpr = seq match {
+    case expr :: Nil => expr
+    case _ => QSeq(seq)
+  }
+}
+case class QDisj(qexprs: Seq[QExpr]) extends QExpr
+case object QDisj {
+  def fromSeq(seq: Seq[QExpr]): QExpr = seq match {
+    case expr :: Nil => expr
+    case _ => QDisj(seq)
+  }
+}
+
+object QExprParser extends RegexParsers {
+  val posTagSet = Seq("PRP$","NNPS","WRB","WP$","WDT","VBZ","VBP","VBN","VBG","VBD","SYM","RBS",
+      "RBR","PRP","POS","PDT","NNS","NNP","JJS","JJR","WP","VB","UH","TO","RP","RB","NN","MD","LS",
+      "JJ","IN","FW","EX","DT","CD","CC")
+  val posTagRegex = posTagSet.map(Pattern.quote).mkString("|").r
+  def word = positioned("""[^|\^$()\s*+]+""".r ^^ QWord)
+  def cluster = positioned("""\^[01]+""".r ^^ { s => QCluster(s.tail) })
+  def pos = positioned(posTagRegex ^^ QPos)
+  def dict = positioned("""\$[^$()\s*+|]+""".r ^^ { s => QDict(s.tail) })
+  def wildcard = positioned("\\.".r ^^^ QWildcard)
+  def atom = positioned(wildcard | pos | dict | cluster | word)
+  def captureName = "?<" ~> """[A-z0-9]+""".r <~ ">"
+  def named = positioned("(" ~> captureName ~ expr <~ ")" ^^ { x => QNamed(x._2, x._1) })
+  def unnamed = positioned("(" ~> expr <~ ")" ^^ QUnnamed)
+  def nonCap = positioned("(?:" ~> expr <~ ")" ^^ QNonCap)
+  def operand = positioned(named | nonCap | unnamed | atom)
+  def starred = positioned(operand <~ "*" ^^ QStar)
+  def plussed = positioned(operand <~ "+" ^^ QPlus)
+  def modified = positioned(starred | plussed)
+  def piece: Parser[QExpr] = positioned((modified | operand))
+  def branch = positioned(rep1(piece) ^^ QSeq.fromSeq)
+  def expr = positioned(repsep(branch, "|") ^^ QDisj.fromSeq)
+  def parse(s: String) = parseAll(expr, s)
+}
---
+++ src/test/scala/org/allenai/dictionary/TestQExprParser.scala
@@ -0,0 +1,60 @@
+package org.allenai.dictionary
+
+import org.allenai.common.testkit.UnitSpec
+import org.allenai.common.testkit.ScratchDirectory
+
+class TestQExprParser extends UnitSpec with ScratchDirectory {
+  
+  val wc = QWildcard
+  def w(s: String) = QWord(s)
+  def p(s: String) = QPos(s)
+  def c(s: String) = QCluster(s)
+  def qs(exprs: QExpr*) = QSeq(exprs)
+  def cap(expr: QExpr) = QUnnamed(expr)
+  def cap(name: String, expr: QExpr) = QNamed(expr, name)
+  def nocap(expr: QExpr) = QNonCap(expr)
+  def or(exprs: QExpr*) = QDisj(exprs)
+  def star(expr: QExpr) = QStar(expr)
+  
+  def parse(s: String): QExpr = QExprParser.parse(s).get
+  
+  "QExprParser" should "parse correctly" in {
+    
+    val q1 = "this is a test"
+    val e1 = qs(w("this"), w("is"), w("a"), w("test"))
+    assert(parse(q1) == e1)
+    
+    val q2 = "this is DT NN"
+    val e2 = qs(w("this"), w("is"), p("DT"), p("NN"))
+    assert(parse(q2) == e2)
+    
+    val q3 = "^10 is DT NN"
+    val e3 = qs(c("10"), w("is"), p("DT"), p("NN"))
+    assert(parse(q3) == e3)
+    
+    val q4 = "this is (a test)"
+    val e4 = qs(w("this"), w("is"), cap(qs(w("a"), w("test"))))
+    assert(parse(q4) == e4)
+    
+    val q5 = "this is (?<foo> a test)"
+    val e5 = qs(w("this"), w("is"), cap("foo", qs(w("a"), w("test"))))
+    assert(parse(q5) == e5)
+    
+    val q6 = "this is (?:a test)"
+    val e6 = qs(w("this"), w("is"), nocap(qs(w("a"), w("test"))))
+    assert(parse(q6) == e6)
+    
+    val q7 = "(?:this|that) is a test"
+    val e7 = qs(nocap(or(w("this"), w("that"))), w("is"), w("a"), w("test"))
+    assert(parse(q7) == e7)
+    
+    val q8 = ". is a test"
+    val e8 = qs(wc, w("is"), w("a"), w("test"))
+    assert(parse(q8) == e8)
+    
+    val q9 = ".* is a test"
+    val e9 = qs(star(wc), w("is"), w("a"), w("test"))
+    assert(parse(q9) == e9)
+    
+  }
+}
