26a163ac17a80478e5d057cc7b364301c86a92be
Tony Fader <afader@gmail.com>
2015-01-20 17:03:21 -0800

    more test

--- src/test/scala/org/allenai/dictionary/TestSearcher.scala
+++ src/test/scala/org/allenai/dictionary/TestSearcher.scala
@@ -8,11 +8,34 @@ import scala.collection.JavaConverters._
 class TestSearcher extends UnitSpec with ScratchDirectory {
   TestData.createTestIndex(scratchDir)
   val searcher = TestData.testSearcher(scratchDir)
-  "searcher" should "return the expected search results" in {
-    val query = CorpusQueryLanguageParser.parse(""" "I" [pos="VBP"] """)
+  def search(s: String): Iterator[BlackLabResult] = {
+    val query = CorpusQueryLanguageParser.parse(s)
     val hits = searcher.find(query)
-    val results = BlackLabResult.fromHits(hits)
+    BlackLabResult.fromHits(hits)
+  }
+  def groups(s: String): Iterator[String] = for {
+    r <- search(s)
+    (name, offset) <- r.captureGroups
+    data = r.wordData.slice(offset.start, offset.end)
+    words = data.map(_.word).mkString(" ")
+    result = s"$name $words"
+  } yield result
+  "searcher" should "return the expected search results" in {
+    val results = search(""" "I" [pos="VBP"] """)
     val resultStrings = results.map(r => r.matchWords.mkString(" ")).toSet
     assert(resultStrings == Set("I like", "I hate"))
+  }
+  it should "handle capture groups" in {
+    val results = groups(""" "I" myGroup:[pos="VBP"] """)
+    assert(results.toSet == Set("myGroup like", "myGroup hate"))
+  }
+  it should "handle multi-word capture groups" in {
+    val results = groups(""" "I" myGroup:([pos="VBP"] [])""")
+    assert(results.toSet == Set("myGroup like mango", "myGroup hate those"))
+  }
+  it should "handle multiple capture groups" in {
+    val results = groups(""" "I" myGroup1:[pos="VBP"] myGroup2:[]""")
+    val expected = Set("myGroup1 like", "myGroup1 hate", "myGroup2 mango", "myGroup2 those")
+    assert(results.toSet == expected)
   }
 }
