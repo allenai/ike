bec112c30a4d55465d217eac90e2a31f99e73920
Tony Fader <afader@gmail.com>
2015-01-28 11:07:10 -0800

    added error and loading

--- webapp/app/js/components/CorpusSearcher.js
+++ webapp/app/js/components/CorpusSearcher.js
@@ -6,6 +6,7 @@ var DictionaryInterface = require('./DictionaryInterface.js');
 var bs = require('react-bootstrap');
 var TabbedArea = bs.TabbedArea;
 var TabPane = bs.TabPane;
+var Alert = bs.Alert;
 
 var CorpusSearcher = React.createClass({
 
@@ -50,6 +51,10 @@ var CorpusSearcher = React.createClass({
     return {
       results: [], 
       groupedResults: [],
+      error: false,
+      errorMessage: null,
+      hasContent: false,
+      loading: false,
       dictionaries: {
         technique: {
           name: 'technique',
@@ -66,6 +71,7 @@ var CorpusSearcher = React.createClass({
   },
 
   executeSearch: function(queryObj) {
+    this.setState({loading: true});
     xhr({
       body: JSON.stringify(queryObj),
       uri: '/api/groupedSearch',
@@ -74,7 +80,23 @@ var CorpusSearcher = React.createClass({
       },
       method: 'POST'
     }, function(err, resp, body) {
-      this.setState({groupedResults: JSON.parse(body)});
+      var updatedState;
+      if (resp.statusCode == 200) {
+        updatedState = {
+          groupedResults: JSON.parse(body),
+          error: false,
+          hasContent: true,
+          loading: false
+        };
+      } else {
+        updatedState = {
+          error: true,
+          hasContent: false,
+          errorMessage: resp.body,
+          loading: false
+        };
+      }
+      this.setState(updatedState);
     }.bind(this));
   },
   render: function() {
@@ -84,25 +106,30 @@ var CorpusSearcher = React.createClass({
       createDictionary: this.createDictionary,
       deleteDictionary: this.deleteDictionary
     };
+    var content;
+    if (this.state.error) {
+      content = <Alert bsStyle="danger">{this.state.errorMessage}</Alert>;
+    } else if (this.state.loading) {
+      content = "Loading...";
+    } else if (this.state.hasContent) {
+      content = <GroupedBlackLabResults results={this.state.groupedResults}/>;
+    } else {
+      content = null;
+    }
     return (
       <section>
         <div className="col-md-4">
           <TabbedArea defaultActiveKey={1} animation={false}>
-
             <TabPane tab="Search" eventKey={1}>
               <SearchInterface callback={this.executeSearch}/>
             </TabPane>
-
             <TabPane tab="Dictionaries" eventKey={2}>
               <DictionaryInterface dictionaries={this.state.dictionaries} callbacks={dictionaryCallbacks} /> 
             </TabPane>
-
           </TabbedArea>
         </div>
         <div className="col-md-8">
-
-          <GroupedBlackLabResults results={this.state.groupedResults}/>
-
+          {content}
         </div>
       </section>
     );
