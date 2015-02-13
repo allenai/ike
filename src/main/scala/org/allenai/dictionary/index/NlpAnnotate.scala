2896134018e04afc03c89661ed9710c8c0ea8129
Tony Fader <afader@gmail.com>
2015-02-13 13:11:53 -0800

    hackingt

--- src/main/scala/org/allenai/dictionary/index/NlpAnnotate.scala
+++ src/main/scala/org/allenai/dictionary/index/NlpAnnotate.scala
@@ -1,17 +1,19 @@
 package org.allenai.dictionary.index
 import org.allenai.nlpstack.segment.defaultSegmenter
 import org.allenai.nlpstack.tokenize.defaultTokenizer
-import org.allenai.nlpstack.postag.defaultPostagger
+//import org.allenai.nlpstack.postag.defaultPostagger
 import org.allenai.nlpstack.lemmatize.MorphaStemmer
 import org.allenai.nlpstack.core.Segment
 import org.allenai.nlpstack.core.Token
 import org.allenai.nlpstack.core.PostaggedToken
 import org.allenai.nlpstack.core.Lemmatized
+import org.allenai.nlpstack.postag.FactoriePostagger
 
 object NlpAnnotate {
+  val tagger = new FactoriePostagger
   def segment(text: String): Seq[Segment] = defaultSegmenter.segment(text).toSeq
   def tokenize(segment: Segment): Seq[Token] = defaultTokenizer.tokenize(segment.text)
-  def postag(tokens: Seq[Token]): Seq[PostaggedToken] = defaultPostagger.postagTokenized(tokens)
+  def postag(tokens: Seq[Token]): Seq[PostaggedToken] = tagger.postagTokenized(tokens)
   def lemmatize(postagged: Seq[PostaggedToken]): Seq[Lemmatized[PostaggedToken]] =
     postagged map MorphaStemmer.lemmatizePostaggedToken
   def annotate(text: String): Seq[Seq[Lemmatized[PostaggedToken]]] = for {
