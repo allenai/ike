a0a9095635f0e698ef32e0733c7bc56e9a2de9da
Christopher Clark <chrisc@allenai.org>
2015-03-23 13:20:55 -0700

    Refactored TestHitsGather into two, correctly named, unittests

--- src/test/scala/org/allenai/dictionary/ml/subsample/TestFuzzySequenceSampler.scala
+++ src/test/scala/org/allenai/dictionary/ml/subsample/TestFuzzySequenceSampler.scala
@@ -1,13 +1,13 @@
 package org.allenai.dictionary.ml.subsample
 
-import nl.inl.blacklab.search.Hits
-import org.allenai.common.testkit.{ ScratchDirectory, UnitSpec }
+import org.allenai.common.testkit.{ScratchDirectory, UnitSpec}
 import org.allenai.dictionary._
 import org.allenai.dictionary.index.TestData
+import nl.inl.blacklab.search.Hits
 
 import scala.collection.JavaConverters._
 
-class TestHitsGatherer extends UnitSpec with ScratchDirectory {
+class TestFuzzySequenceSampler extends UnitSpec with ScratchDirectory {
 
   TestData.createTestIndex(scratchDir)
   val searcher = TestData.testSearcher(scratchDir)
@@ -36,11 +36,6 @@ class TestHitsGatherer extends UnitSpec with ScratchDirectory {
         }
       }).toSeq
     }).toSeq
-  }
-
-  def hitsToStrings(hits: Hits): Seq[String] = {
-    hits.iterator.asScala.map(hit =>
-      hits.getKwic(hit).getMatch("word").asScala.mkString(" ")).toSeq
   }
 
   def buildTable(positive: Seq[String], negative: Seq[String]): Table = {
@@ -75,28 +70,5 @@ class TestHitsGatherer extends UnitSpec with ScratchDirectory {
     assertResult(Seq("mango", null, null, "mango"))(captures(0)) // Last word did not match
     assertResult(Seq("those", null, null, null))(captures(1)) // all words matched
     assertResult(Seq("great", null, "not", null))(captures(2)) // middle word did not match
-  }
-
-  "LimitQuery" should "Convert wildcards captures into a disjunction" in {
-    val startingQuery = QSeq(Seq(QWord("the"), QUnnamed(QSeq(Seq(QWildcard(), QWildcard())))))
-    val table = buildTable(Seq("a b", "d"), Seq("c", "1 2 3", "a c"))
-    val expectedResults = QSeq(Seq(QWord("the"), QUnnamed(QDisj(Seq(
-      QSeq(Seq(QWord("a"), QWord("b"))),
-      QSeq(Seq(QWord("a"), QWord("c")))
-    )))))
-    assertResult(expectedResults)(
-      MatchesSampler().limitQueryToDictionary(startingQuery, table)
-    )
-  }
-
-  "MatchesSampler" should "test correctly" in {
-    val startingQuery = QUnnamed(QDisj(Seq(QWord("I"), QWord("mango"),
-      QWord("not"), QWord("great"))))
-    val table = buildTable(Seq("like", "hate", ".*", "not"), Seq("mango", "I"))
-    val expectedResults = Seq("I", "mango", "I", "not")
-
-    assertResult(expectedResults)(hitsToStrings(MatchesSampler().getLabelledSample(
-      startingQuery, searcher, table
-    )))
   }
 }
