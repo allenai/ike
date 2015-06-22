368e84cf56f5184af89f7b5d38c520e5330973b7
Dirk Groeneveld <dirkg@allenai.org>
2015-06-22 10:27:21 -0700

    Formatting

--- src/main/scala/org/allenai/dictionary/SearchResultGrouper.scala
+++ src/main/scala/org/allenai/dictionary/SearchResultGrouper.scala
@@ -58,14 +58,13 @@ object SearchResultGrouper extends Logging {
     }
     val grouped = keyed groupBy keyString
 
-    /**
-     * Checks if subsetKey is a subset of supersetKey.
-     *
-     * ["sky", "blue and white"] is a subset of
-     * ["big sky", "blue and white during the day"]. A key is always a subset of
-     * itself. Column order matters, so ["a", "b"] is not a subset of
-     * ["b", "a"].
-     */
+    /** Checks if subsetKey is a subset of supersetKey.
+      *
+      * ["sky", "blue and white"] is a subset of
+      * ["big sky", "blue and white during the day"]. A key is always a subset of
+      * itself. Column order matters, so ["a", "b"] is not a subset of
+      * ["b", "a"].
+      */
     def isSubset(subsetKey: Seq[Phrase], supersetKey: Seq[Phrase]): Boolean = {
       require(subsetKey.length == supersetKey.length)
       (subsetKey zip supersetKey).forall {
