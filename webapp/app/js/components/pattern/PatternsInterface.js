9552e6b104cc72a781670cb1d49d9e6cd28ca774
Dirk Groeneveld <dirkg@allenai.org>
2015-07-15 15:27:43 -0700

    Uses the new patterns database API

--- webapp/app/js/components/pattern/PatternsInterface.js
+++ webapp/app/js/components/pattern/PatternsInterface.js
@@ -7,7 +7,9 @@ var NavItem = bs.NavItem;
 var Row = bs.Row;
 var Col = bs.Col;
 var Input = bs.Input;
+var Well = bs.Well;
 var PatternEditor = require('./PatternEditor.js');
+const AuthStore = require('../../stores/AuthStore.js');
 
 var PatternsInterface = React.createClass({
   mixins: [React.addons.LinkedStateMixin],
@@ -25,42 +27,56 @@ var PatternsInterface = React.createClass({
     };
   },
 
-  componentDidMount: function() {
+  updatePatternsFromServer: function() {
+    this.setState(this.getInitialState());
+
     var self = this;
+    var userEmail = AuthStore.getUserEmail();
 
     // Get the patterns from the server
-    xhr({
-      uri: '/api/patterns',
-      method: 'GET'
-    }, function(err, resp, body) {
-      if (self.isMounted()) {
-        if (resp.statusCode == 200) {
-          // set patterns
-          var patternsObjects = JSON.parse(body);
-          var patterns = {};
-          patternsObjects.forEach(function (patternObject) {
-            patterns[patternObject.name] = patternObject.pattern;
-          });
+    if(userEmail) {
+      xhr({
+        uri: '/api/patterns/' + encodeURIComponent(userEmail),
+        method: 'GET'
+      }, function(err, resp, body) {
+        if (self.isMounted()) {
+          if (resp.statusCode == 200) {
+            // set patterns
+            var patternsObjects = JSON.parse(body);
+            var patterns = {};
+            patternsObjects.forEach(function (patternObject) {
+              patterns[patternObject.name] = patternObject.pattern;
+            });
 
-          // set active pattern name
-          var activePatternName = self.state.activePatternName;
-          if (activePatternName && !patterns.hasOwnProperty(activePatternName))
-            activePatternName = null;
+            // set active pattern name
+            var activePatternName = self.state.activePatternName;
+            if (activePatternName && !patterns.hasOwnProperty(activePatternName))
+              activePatternName = null;
 
-          if (!activePatternName)
-            activePatternName = patternsObjects.length > 0 ? patternsObjects[0].name : null;
+            if (!activePatternName)
+              activePatternName = patternsObjects.length > 0 ? patternsObjects[0].name : null;
 
-          self.setState({
-            patterns: patterns,
-            activePatternName: activePatternName,
-            error: null
-          });
-        } else {
-          self.setState({error: resp.body + " (" + resp.statusCode + ")"});
-          console.log(resp);
+            self.setState({
+              patterns: patterns,
+              activePatternName: activePatternName,
+              error: null
+            });
+          } else {
+            self.setState({error: resp.body + " (" + resp.statusCode + ")"});
+            console.log(resp);
+          }
         }
-      }
-    });
+      });
+    }
+  },
+
+  componentDidMount: function() {
+    AuthStore.addChangeListener(this.updatePatternsFromServer);
+    this.updatePatternsFromServer();
+  },
+
+  componentWillUnmount: function() {
+    AuthStore.removeChangeListener(this.updatePatternsFromServer);
   },
 
   patternClicked: function(patternName) {
@@ -96,6 +112,8 @@ var PatternsInterface = React.createClass({
       return <Alert bsStyle="danger">{this.state.error}</Alert>
     } else {
       var self = this;
+
+      // build the list of patterns
       var items = [];
       for(var patternName in this.state.patterns) {
         if(this.state.patterns.hasOwnProperty(patternName)) {
@@ -104,6 +122,13 @@ var PatternsInterface = React.createClass({
               {patternName}
             </NavItem>);
         }
+      }
+
+      if(items.length === 0) {
+        if(AuthStore.getUserEmail())
+          items = <Well>It looks like you have no patterns defined yet.</Well>;
+        else
+          items = <Well>You must be signed in to create named patterns.</Well>;
       }
 
       var patternChooser = <div>
@@ -120,6 +145,7 @@ var PatternsInterface = React.createClass({
                  help='Enter a name for a new pattern and press enter.'
                  valueLink={this.linkState('newPatternName')}
                  bsStyle={this.newPatternNameValidationState()}
+                 disabled={!AuthStore.getUserEmail()}
                  hasFeedback />
         </form>
         </div>;
