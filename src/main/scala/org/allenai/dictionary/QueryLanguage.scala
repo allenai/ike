bf4063c768cc59a616929688e59225e8850ae55a
Dirk Groeneveld <dirkg@allenai.org>
2015-07-23 16:08:24 -0700

    Making it so . needs to be escaped
    This is not strictly necessary, but it's strange that it was the only special character that didn't need escaping.

--- src/main/scala/org/allenai/dictionary/QueryLanguage.scala
+++ src/main/scala/org/allenai/dictionary/QueryLanguage.scala
@@ -74,7 +74,7 @@ object QExprParser extends RegexParsers {
   // Turn off style---these are all just Parser[QExpr] definitions
   // scalastyle:off
   def integer = """-?[0-9]+""".r ^^ { _.toInt }
-  def wordRegex = """(\\.|[^|\]\[\^(){}\s*+,"~])+""".r
+  def wordRegex = """(\\.|[^|\]\[\^(){}\s*+,."~])+""".r
   def word = wordRegex ^^ { x => QWord(x.replaceAll("""\\(.)""", """$1""")) }
   def generalizedWord = (word <~ "~") ~ integer ^^ { x =>
     QGeneralizePhrase(Seq(x._1), x._2)
