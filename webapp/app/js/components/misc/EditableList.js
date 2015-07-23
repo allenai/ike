923195eaeb75e82eeaeb418594c4d7204aada41b
Dirk Groeneveld <dirkg@allenai.org>
2015-07-22 18:03:39 -0700

    Formatting

--- webapp/app/js/components/misc/EditableList.js
+++ webapp/app/js/components/misc/EditableList.js
@@ -1,16 +1,19 @@
-var React = require('react/addons');
-var bs = require('react-bootstrap');
-var DeleteButton = require('./DeleteButton.js');
-var Input = bs.Input;
-var ListGroup = bs.ListGroup;
-var ListGroupItem = bs.ListGroupItem;
+const React = require('react/addons');
+const bs = require('react-bootstrap');
+const DeleteButton = require('./DeleteButton.js');
+const Input = bs.Input;
+const ListGroup = bs.ListGroup;
+const ListGroupItem = bs.ListGroupItem;
+
 var EditableList = React.createClass({
   getInitialState: function() {
     return {input: ""};
   },
+
   pressedEnterKey: function(e) {
     return e.which == 13;
   },
+
   handleChange: function(e) {
     if (this.pressedEnterKey(e)) {
       this.add();
@@ -18,22 +21,27 @@ var EditableList = React.createClass({
       this.setState({input: e.target.value});
     }
   },
+
   add: function() {
     var input = this.state.input;
     this.props.onAdd(input);
     this.setState({input: ''});
   },
+
   remove: function(i) {
     return function () { this.props.onRemove(i); }.bind(this);
   },
+
   focus: function() {
     this.refs.inputBox.getDOMNode().childNodes[0].focus();
   },
+
   makeRow: function(value, i) {
     var button = <DeleteButton callback={this.remove(i)}/>;
     var key = value + '.' + i;
     return <ListGroupItem key={key}>{value} {button}</ListGroupItem>;
   },
+
   render: function() {
     var name = this.props.name;
     var items = this.props.value;
@@ -51,5 +59,6 @@ var EditableList = React.createClass({
       </div>
     );
   }
+
 });
 module.exports = EditableList;
