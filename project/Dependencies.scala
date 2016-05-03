ac7e6329c35d1f3bc8036628400e662f47b5e4b5
sbhaktha <sumithrab@allenai.org>
2016-05-03 07:36:38 -0700

    Switch Datastore dependency to public version from Maven

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies extends CoreDependencies {
   val luceneGroup = "org.apache.lucene"
   val luceneVersion = "4.2.1"
   def lucene(part: String) = luceneGroup % s"lucene-${part}" % luceneVersion
-  val allenAiDatastore = "org.allenai" %% "datastore" % "2015.04.02-0"
+  val allenAiDatastore = "org.allenai.datastore" %% "datastore" % "1.0.7"
   val nlpstackVersion = "1.10"
   def nlpstackModule(id: String) = "org.allenai.nlpstack" %% s"nlpstack-${id}" % nlpstackVersion
 }
