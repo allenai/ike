690124ba26c226d3fb29571bcd4f7b9f982fe4a5
sbhaktha <sumithrab@allenai.org>
2016-05-16 14:41:42 -0700

    Remove passwords from conf files

---
+++ src/main/bin/webapp-prod.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Look for a production password file, and source it if it exists.
+CREDENTIALS=/opt/ops/var/s3/ops-keystore/database/ike/pgpass-prod.sh
+if [ -e "$CREDENTIALS" ]; then
+  source "$CREDENTIALS"
+
+# Regular stuff below
+  JVM_ARGS="-Xmx60G -Xms60G"
+  CLASS_NAME="org.allenai.ike.IkeToolWebapp"
+  SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+  SHORT_NAME=webapp
+  . "${SCRIPT_DIR}/run-class.sh" "$CLASS_NAME" "$SHORT_NAME" "$@"
+else
+  echo "Warning: $CREDENTIALS not found; will fail to start in prod."
+fi
--- src/main/bin/webapp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Regular stuff below
-JVM_ARGS="-Xmx60G -Xms60G"
-CLASS_NAME="org.allenai.ike.IkeToolWebapp"
-SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-SHORT_NAME=webapp
-. "${SCRIPT_DIR}/run-class.sh" "$CLASS_NAME" "$SHORT_NAME" "$@"
