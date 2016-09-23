68cae68778c1bd7591065c7b0a23ec8e5c5e6953
Hiram "Hye" Brown <hiramhye@users.noreply.github.com>
2016-09-23 16:17:43 -0700

    Ike control file update (#218)
    * adding and updating ike control scripts
    
    * removing files that have been replaced
    
    * adding Ingmars proposed changes
    
    * missed one

--- src/main/bin/prepare-env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-ROOT_DIR=`dirname $SCRIPT_DIR`
--- src/main/bin/webapp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-JVM_ARGS="-Xmx60G -Xms60G"
-CLASS_NAME="org.allenai.ike.IkeToolWebapp"
-SHORT_NAME=webapp
-. "${SCRIPT_DIR}/run-class.sh" "$CLASS_NAME" "$SHORT_NAME" "$@"
